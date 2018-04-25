74673cea8b0bbda37bdfc29c71ade31feb623e47
xiaotaoma <xiaotao.ma.zbj@gmail.com>
2018-04-25 22:24:33 +0800

    11

--- src/main/java/com/mxt/springcloud/EurekaServerApplication.java
+++ src/main/java/com/mxt/springcloud/EurekaServerApplication.java
@@ -6,11 +6,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Created by mxt on 18-3-6.
+ * 服务注册中心
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class Application {
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
+        new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
     }
 }
